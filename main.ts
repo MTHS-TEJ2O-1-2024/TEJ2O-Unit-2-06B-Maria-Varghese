767e294012bbbf34aacde24f4e93a034d27e616e
Maria-Varghese <maria.varghese@mths.ca>
2024-09-29 06:03:16 -0700

    Turning the LED on and off

--- main.ts
+++ main.ts
@@ -1,8 +1,23 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Maria
+ * Created on: Sep 2024
+ * This program turns the LED on and off
 */
 
-basic.showString('Hello, World!')
+//setup
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+pins.digitalWritePin(DigitalPin.P19, 0)
+
+//turn on the LED
+input.onButtonPressed(Button.A, function() {
+    basic.showIcon(IconNames.Yes)
+    pins.digitalWritePin(DigitalPin.P19, 1)
+})
+
+//turn on the LED
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.No)
+    pins.digitalWritePin(DigitalPin.P19, 0)
+})
