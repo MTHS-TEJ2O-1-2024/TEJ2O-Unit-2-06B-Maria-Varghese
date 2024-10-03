82be791d25068259022afeae727020b32292ace4
Maria-Varghese <maria.varghese@mths.ca>
2024-10-03 08:23:31 -0700

    turning the Led On and Off

--- main.ts
+++ main.ts
@@ -8,16 +8,16 @@
 //setup
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
-pins.digitalWritePin(DigitalPin.P19, 0)
+pins.digitalWritePin(DigitalPin.P16, 0)
 
 //turn on the LED
 input.onButtonPressed(Button.A, function() {
     basic.showIcon(IconNames.Yes)
-    pins.digitalWritePin(DigitalPin.P19, 1)
+    pins.digitalWritePin(DigitalPin.P16, 1)
 })
 
 //turn on the LED
 input.onButtonPressed(Button.B, function () {
     basic.showIcon(IconNames.No)
-    pins.digitalWritePin(DigitalPin.P19, 0)
+    pins.digitalWritePin(DigitalPin.P16, 0)
 })
